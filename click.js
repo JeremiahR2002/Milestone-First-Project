0be2132297844e2bd2a99c73dcdb12c1360682fe
Jeremiah Reid <jeremiahdelacruzreid@gmail.com>
2022-08-02 15:10:01 -0400

    Added text to if you dont have enough cookies and opacity changes

--- click.js
+++ click.js
@@ -6,6 +6,8 @@ function main() {
     let cookieamount = 0;
     let cookiestand = document.querySelector('.stand');
     let cookiefarm = document.querySelector('.farm');
+    let sorry1 = document.querySelector('.sorry1');
+    let sorry2 = document.querySelector('.sorry2');
     // function increasecookies(){
     //     cookieamount++;
     //     currentcookies.textContent = cookieamount;
@@ -29,7 +31,9 @@ function main() {
             10000);
         }
        } else {
+        sorry1.style.opacity = '100%';
         console.log("Not Enough Cookies to Buy!");
+        sorry()
        }
     });
 
@@ -38,16 +42,29 @@ function main() {
          console.log("second upgrade purchased!");
          cookieamount -= 150;
          currentcookies.textContent = cookieamount;
-         cookiefarm.style.opacity = '100%'
+         cookiefarm.style.opacity = '100%';
          for (var i = 0; i < 1; i++) {
              setInterval(() => {cookieamount += 15, currentcookies.textContent = cookieamount; }, 
              20000);
          }
         } else {
+         sorry2.style.opacity = '100%';
          console.log("Not Enough Cookies to Buy!");
+         sorry3()
         }
      });
 
+async function sorry() {
+    setTimeout(() => { 
+        sorry1.style.opacity = '0';
+    }, 5000);
+}
+
+async function sorry3() {
+    setTimeout(() => {
+       sorry2.style.opacity = '0'; 
+    }, 5000);
+}
      // audio did not work here for some odd reason, had to script it in html
     // let cookiemusic = document.getElementById("cookiemusic");
     // function play() {
