1a418679312df3af2730564091729fe2416db4d3
Jeremiah Reid <jeremiahdelacruzreid@gmail.com>
2022-08-02 17:56:31 -0400

    Added Indicators when the stand gives cookies

--- click.js
+++ click.js
@@ -8,6 +8,8 @@ function main() {
     let cookiefarm = document.querySelector('.farm');
     let sorry1 = document.querySelector('.sorry1');
     let sorry2 = document.querySelector('.sorry2');
+    let obtainedcookies = document.querySelector('.cookieobtained');
+    let obtainedcookies2 = document.querySelector('.cookiesobtained2')
     // function increasecookies(){
     //     cookieamount++;
     //     currentcookies.textContent = cookieamount;
@@ -27,8 +29,8 @@ function main() {
         currentcookies.textContent = cookieamount;
         cookiestand.style.opacity = '100%';
         for (var i = 0; i < 1; i++) {
-            setInterval(() => {cookieamount += 5, currentcookies.textContent = cookieamount; }, 
-            10000);
+            setInterval(() => {cookieamount += 5, obtainedcookies.style.opacity = '100%', obtained(), currentcookies.textContent = cookieamount; }, 
+            5000);
         }
        } else {
         sorry1.style.opacity = '100%';
@@ -38,14 +40,14 @@ function main() {
     });
 
     upgrade2.addEventListener('click', function() {
-        if (cookieamount >= 150) {
+        if (cookieamount >= 100) {
          console.log("second upgrade purchased!");
-         cookieamount -= 150;
+         cookieamount -= 100;
          currentcookies.textContent = cookieamount;
          cookiefarm.style.opacity = '100%';
          for (var i = 0; i < 1; i++) {
-             setInterval(() => {cookieamount += 15, currentcookies.textContent = cookieamount; }, 
-             20000);
+             setInterval(() => {cookieamount += 15, obtainedcookies2.style.opacity = '100%', obtained2(), currentcookies.textContent = cookieamount; }, 
+             10000);
          }
         } else {
          sorry2.style.opacity = '100%';
@@ -56,14 +58,26 @@ function main() {
 
 async function sorry() {
     setTimeout(() => { 
-        sorry1.style.opacity = '0';
-    }, 5000);
+        sorry1.style.opacity = '0%';
+    }, 3000);
 }
 
 async function sorry3() {
     setTimeout(() => {
-       sorry2.style.opacity = '0'; 
-    }, 5000);
+       sorry2.style.opacity = '0%'; 
+    }, 3000);
+}
+
+async function obtained() {
+    setTimeout(() => {
+        obtainedcookies.style.opacity='0%';
+    }, 250)
+}
+
+async function obtained2() {
+    setTimeout(() => {
+        obtainedcookies2.style.opacity='0%';
+    }, 250)
 }
      // audio did not work here for some odd reason, had to script it in html
     // let cookiemusic = document.getElementById("cookiemusic");
